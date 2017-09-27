75fd40cee79752421d7c948200d6e0aa23f969d7
Fryland Alganja <mccmik14@evergreen.edu>
2017-09-26 19:40:32 -0700

    Added an assets folder (img/) and the spritesheet to the repo, instead of pulling the spritesheet from the former version of this game's repo.

--- piggle.js
+++ piggle.js
@@ -1,5 +1,5 @@
 Crafty.init(500,350, document.getElementById('game'));
-Crafty.sprite("https://raw.githubusercontent.com/RylandAlmanza/piggle/master/img/sprites.png", {guyNorth: [0, 0, 32, 32], guyEast: [32, 0, 32, 32], guySouth: [64, 0, 32, 32], guyWest: [96, 0, 32, 32]});
+Crafty.sprite("img/sprites.png", {guyNorth: [0, 0, 32, 32], guyEast: [32, 0, 32, 32], guySouth: [64, 0, 32, 32], guyWest: [96, 0, 32, 32]});
 var playerSpeed = 64;
 Crafty.e('2D, DOM, Color, guyNorth, Motion, Keyboard')
     .attr({x: 0, y: 0, w: 32, h: 32})
